b3365c0c7b9c11f5a04d01fdd6e12699f9939bb8
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-10-21 22:15:31 +0000

    Aula 21/10 4

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,11 +7,11 @@ async function visualizarInformacoesGlobais() {
     
     console.log(dados);
 
-    const paragrafo = document.createElement('p');
+    const paragrafo = document.createElement("p");
     paragrafo.classList.add('graficos-container__texto');
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`;
     
-    const container = document.getElementById('graficos-container');
+    const container = document.getElementById("graficos-container");
     container.appendChild(paragrafo);
     
 }
