5a7018c4867f62e6e228d5b7804282a68bb593af
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-04 18:49:23 -0300

    7

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,10 +6,11 @@ async function visualizarInformacoesGlobais() {
     const dados = await resposta.json();
     
     //console.log(dados);
+    let total_pessoas_mundo = dados.total_pessoas_mundo/1e9
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. <br> <br> 
+    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${dados.total_pessoas_mundo.toFixed(2)}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. <br> <br> 
     
     Isso significa que aproximadamente <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. <br> <br> 
     
