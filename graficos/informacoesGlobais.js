37f47387fefcac04b48566cd5a0868d4e6ab1340
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-04 18:31:12 -0300

    Aula 04/11 4

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,11 +9,11 @@ async function visualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. <br> 
+    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. <br> <br> 
     
-    Isso significa que aproximadamente <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. <br> 
+    Isso significa que aproximadamente <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. <br> <br> 
     
-    Isso reflete não apenas a importância da infraestrutura de mobilidade, mas também o impacto que o transporte tem na qualidade de vida e na eficiência laboral. <br> 
+    Isso reflete não apenas a importância da infraestrutura de mobilidade, mas também o impacto que o transporte tem na qualidade de vida e na eficiência laboral. <br> <br> 
     
     Esse dado também pode ser útil para embasar discussões sobre sustentabilidade, urbanização e políticas públicas voltadas para melhorar os meios de transporte, como o incentivo a opções mais sustentáveis ou a criação de alternativas para reduzir o tempo gasto em deslocamentos.`;
     
