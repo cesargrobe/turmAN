e30cd483f840a30fd93c89cecc5ab8cd61a2d4ce
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-04 18:25:34 -0300

    Aula 04/11 2

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,15 +1,15 @@
-const url=`https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json`
+const url=`https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json`
 
 async function visualizarInformacoesGlobais() {
 
     const resposta = await fetch(url);
     const dados = await resposta.json();
     
-    console.log(dados);
+    //console.log(dados);
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>"${dados.total_pessoas_mundo}"</span> e que aproximadamente <span>"${dados.total_pessoas_conectadas}"</span> estão conectadas em alguma rede social e passam em média <span>"${dados.tempo_medio}"</span> horas conectadas.`;
+    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${dados.total_pessoas_mundo}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> minutos. \nIsso significa que aproximadamente <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. \nIsso reflete não apenas a importância da infraestrutura de mobilidade, mas também o impacto que o transporte tem na qualidade de vida e na eficiência laboral. \nEsse dado também pode ser útil para embasar discussões sobre sustentabilidade, urbanização e políticas públicas voltadas para melhorar os meios de transporte, como o incentivo a opções mais sustentáveis ou a criação de alternativas para reduzir o tempo gasto em deslocamentos.`;
     
     const container = document.getElementById("graficos-container");
     container.appendChild(paragrafo);
