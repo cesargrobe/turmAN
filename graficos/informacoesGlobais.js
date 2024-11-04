510870ee380438780400f58f08a1f1b1430cc956
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-04 19:06:07 -0300

    14

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -13,9 +13,9 @@ async function visualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement("p");
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${total_pessoas_mundo.toFixed(2)}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${tempo_medio_deslocamento_para_trabalho.toFixed(2)} </span> minutos. <br> <br> 
+    paragrafo.innerHTML = `O trabalho agrega valor e propósito à vida das pessoas. Atualmente existem cerca de <span>${total_pessoas_mundo.toFixed(2)}</span> bilhões de pessoas no mundo, destes, cerca de 2 bilhões de pessoas precisam de um meio de transporte para chegar ao seu trabalho levando em média <span>${tempo_medio_deslocamento_para_trabalho} </span> minutos. <br> <br> 
     
-    Isso significa que aproximadamente <span>${total_pessoas_que_necessitam_transporte_para_trabalho}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. <br> <br> 
+    Isso significa que aproximadamente <span>${total_pessoas_que_necessitam_transporte_para_trabalho.toFixed(2)}</span> das pessoas dependem de algum meio de transporte para trabalhar. Considerando a média de tempo para chegar ao trabalho, cerca de 13% do tempo diário é passado em um meio de transporte. <br> <br> 
     
     Isso reflete não apenas a importância da infraestrutura de mobilidade, mas também o impacto que o transporte tem na qualidade de vida e na eficiência laboral. <br> <br> 
     
