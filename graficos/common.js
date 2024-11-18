6328a614426b026a95ea5cd8318c728c0e67c659
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-11-18 19:22:33 -0300

    8

--- graficos/common.js
+++ graficos/common.js
@@ -3,7 +3,7 @@ const getCSS = (variavel) => {
 }
 const tickConfig = {
     color: getCSS('--primary-color'),
-    size: 16,
+    size: 14,
     family: getCSS('--font')
 }
 export {getCSS, tickConfig}
